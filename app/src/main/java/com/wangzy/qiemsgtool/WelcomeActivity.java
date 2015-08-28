604c952bd6b1c56058532932d14fef3fa8feb1b9
wangzy <wzyfly@sina.com>
2015-08-29 00:00:22 +0800

    add welcome msg

---
+++ app/src/main/java/com/wangzy/qiemsgtool/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.wangzy.qiemsgtool;
+
+import android.os.Bundle;
+
+import com.common.BaseActivity;
+
+public class WelcomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+
+}

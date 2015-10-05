9ee4d97870456f2335656d41e3bb008da2b2a497
wangzy <wzyfly@sina.com>
2015-10-05 13:44:06 +0800

    no message

--- app/src/main/java/com/wangzy/qiemsgtool/WelcomeActivity.java
+++ app/src/main/java/com/wangzy/qiemsgtool/WelcomeActivity.java
@@ -1,13 +1,17 @@
 package com.wangzy.qiemsgtool;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 
 import com.common.BaseActivity;
+import com.common.util.DialogCallBackListener;
 import com.common.util.DialogUtils;
 import com.common.util.Root;
 import com.common.util.Tool;
+
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -49,6 +53,30 @@ public class WelcomeActivity extends BaseActivity {
             Tool.startActivity(this, MainActivity.class);
         } else {
 
+            String title = getResources().getString(R.string.main_not_root);
+            String msg = getResources().getString(R.string.main_search_root);
+
+            String yes = getResources().getString(R.string.confirm);
+            String no = getResources().getString(R.string.cancell);
+
+            DialogUtils.showConfirmDialog(this, title, msg, yes, no, new DialogCallBackListener() {
+
+                @Override
+                public void onDone(boolean yesOrNo) {
+
+
+//                    android.os.Build.MODEL
+
+                    if (yesOrNo) {
+
+                        searchRoot(Build.MODEL);
+                    } else {
+
+
+                    }
+
+                }
+            });
 
 
         }
@@ -56,4 +84,34 @@ public class WelcomeActivity extends BaseActivity {
 
     }
 
+
+    private void searchRoot(String model) {
+
+
+        if (isZh()) {
+
+            String url = "https://www.baidu.com/s?wd=root%20" + model;
+
+            Tool.startUrl(this, url);
+
+        } else {
+
+            String url = "https://www.google.com/search?q=how+to+root+nexus&oq=how+to+root+" + model;
+
+            Tool.startUrl(this, url);
+
+        }
+
+
+    }
+
+
+    private boolean isZh() {
+        Locale locale = getResources().getConfiguration().locale;
+        String language = locale.getLanguage();
+        if (language.endsWith("zh"))
+            return true;
+        else
+            return false;
+    }
 }
